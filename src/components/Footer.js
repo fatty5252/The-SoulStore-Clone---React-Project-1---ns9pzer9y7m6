5619fd53afc1c943a3542cecba54367690d2e0fe
Fatema <fatema.rubberstampwala@gmail.com>
2024-05-13 19:53:52 +0530

    footer updated for responcive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../styles/Footer.css';
 import { useNavigate } from 'react-router-dom';
+import { useUser } from '../providers/UserProvider';
 
 
 export default function Footer() {
-  const navigate = useNavigate()
-  // function navigattocomingsoon(event){
-  //   navigate
-  // }
-  return (
+  const navigate = useNavigate();
+const {isScreenSmall } = useUser();
+ 
+  return ( <>
+   {isScreenSmall &&
     <div>
       <h2 className='top-footer-heading'>HOMEGROWN INDIAN BRAND</h2>
       <h3 className='bottom-footer-heading'>Over <span className='heading-sub'>6 Million</span> Happy Customers</h3>
@@ -59,6 +60,57 @@ export default function Footer() {
       </div>
       <div style={{textAlign:'center'}}> © The Souled Store made by Fatema 2024-25</div>
       
-    </div>
+    </div>}
+
+    {!isScreenSmall && <div>
+      <h2 className='top-footer-heading'>HOMEGROWN INDIAN BRAND</h2>
+      <h3 className='bottom-footer-heading'>Over <span className='heading-sub'>6 Million</span> Happy Customers</h3>
+      <div onClick={(event) => {
+            event.target.tagName === 'P' && navigate('/ComingSoon');
+          }} className='main-footer'>
+        <div className='sub-footer'>
+          <div className='red-head'>NEED HELP</div>
+          <div>
+            <p>Contact Us</p>
+            <p>Track Order</p>
+            <p>Rteurns & Refunds</p>
+            <p>FAQs</p>
+            <p>My Account</p>
+          </div>
+        </div>
+        <div className='sub-footer'>
+          <div className='red-head'>MORE INFO</div>
+          <p>T&C</p>
+          <p>Privacy Policy</p>
+          <p>Sitemap</p>
+        </div>
+        
+        <div className='sub-footer'>
+          <div className='red-head'>COMPANY</div>
+          <p>About Us</p>
+          <p>Careers</p>
+          <p>Community initiatives</p>
+          <p>Souled Army</p>
+        </div>
+        <div className='sub-footer'>
+          <div className='red-head'>STORE NEAR ME</div>
+          <p>Bandra</p>
+          <p>Thane</p>
+          <p>Colaba</p>
+          <p>Palladium</p>
+          <p>Malad</p>
+          <p>Pune</p>
+          <p>Bnaglore</p>
+          <p>Indore</p>
+          <p>Haryana</p>
+          <p>Bhopal</p>
+          <p>Surat</p>
+        </div>
+        
+      </div>
+      <div style={{textAlign:'center'}}> © The Souled Store made by Fatema 2024-25</div>
+      
+    </div>}
+    </>
   )
 }
